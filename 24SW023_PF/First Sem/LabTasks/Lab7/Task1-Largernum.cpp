a217f7887fe70852ad4dcc29d33147ce83e5e581
IamAntiHero <antihero.void@gmail.com>
2024-12-04 00:18:01 +0500

    Update Task1-Largernum.cpp

--- 24SW023_PF/First Sem/LabTasks/Lab7/Task1-Largernum.cpp
+++ 24SW023_PF/First Sem/LabTasks/Lab7/Task1-Largernum.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
 
+int Largernum(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int main() {
+    cout << Largernum(5, 10) << endl;
+    cout << Largernum(20, 15) << endl;
+    return 0;
+}
