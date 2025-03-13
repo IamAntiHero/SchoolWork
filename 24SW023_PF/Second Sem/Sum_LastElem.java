480bc66d43ca6c72c393cc9c8615340c88774197
IamAntiHero <antihero.void@gmail.com>
2025-03-13 12:57:22 +0500

    Update Sum_LastElem.java

--- 24SW023_PF/Second Sem/Sum_LastElem.java
+++ 24SW023_PF/Second Sem/Sum_LastElem.java
@@ -1,13 +1,9 @@
-public static void main(String args[]) {
-        int[] arr1 = {1, 2, 3, 4, 5};
-        
-        System.out.println("First arr " + Arrays.toString(arr1));
-        
-        int[] arr2 = new int[arr1.length - 2];
-        
+PSVM(String args[]) {
+        int[] arr1 = {1, 2, 3, 4, 5}
+        System.out.println("1 arr " + Arrays.toString(arr1));
+        int[] arr2 = new int[arr1.length - 2
         for(int i = 0; i < arr1.length - 2; i++) {
             arr2[i] = arr1[i] + arr1[i + 1];
         }
-        
-        System.out.println("Second arr " + Arrays.toString(arr2));
+        System.out.println("2 arr " + Arrays.toString(arr2));
     }
