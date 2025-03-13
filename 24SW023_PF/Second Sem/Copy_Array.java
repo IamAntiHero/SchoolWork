25aa312543d7104d6ab3b6c55a1995b4a5c181de
IamAntiHero <antihero.void@gmail.com>
2025-03-13 12:34:11 +0500

    Update Copy_Array.java

--- 24SW023_PF/Second Sem/Copy_Array.java
+++ 24SW023_PF/Second Sem/Copy_Array.java
@@ -4,50 +4,50 @@ import java.util.List;
 
 public class ArrayCopyDemo {
     public static void main(String[] args) {
-        int[] sourceArray = {1, 2, 3, 4, 5};
-        System.out.println("Original Array: " + Arrays.toString(sourceArray));
+        int[] myarr = {1, 2, 3, 4, 5};
+        System.out.println("My Array: " + Arrays.toString(myarr));
         
-        int[] destination1 = Arrays.copyOf(sourceArray, sourceArray.length);
-        System.out.println("1. Arrays.copyOf(): " + Arrays.toString(destination1));
+        int[] copy1 = Arrays.copyOf(myarr, myarr.length);
+        System.out.println("1st way: " + Arrays.toString(copy1));
         
-        int[] destination2 = new int[sourceArray.length];
-        System.arraycopy(sourceArray, 0, destination2, 0, sourceArray.length);
-        System.out.println("2. System.arraycopy(): " + Arrays.toString(destination2));
+        int[] copy2 = new int[myarr.length];
+        System.arraycopy(myarr, 0, copy2, 0, myarr.length);
+        System.out.println("2nd waay: " + Arrays.toString(copy2));
         
-        int[] destination3 = sourceArray.clone();
-        System.out.println("3. clone(): " + Arrays.toString(destination3));
+        int[] copy3 = myarr.clone();
+        System.out.println("3rd way: " + Arrays.toString(copy3));
         
-        int[] destination4 = Arrays.copyOfRange(sourceArray, 0, sourceArray.length);
-        System.out.println("4. Arrays.copyOfRange(): " + Arrays.toString(destination4));
+        int[] copy4 = Arrays.copyOfRange(myarr, 0, myarr.length);
+        System.out.println("4th way: " + Arrays.toString(copy4));
         
-        int[] destination5 = new int[sourceArray.length];
-        for (int i = 0; i < sourceArray.length; i++) {
-            destination5[i] = sourceArray[i];
+        int[] copy5 = new int[myarr.length];
+        for (int i = 0; i < myarr.length; i++) {
+            copy5[i] = myarr[i];
         }
-        System.out.println("5. Manual loop: " + Arrays.toString(destination5));
+        System.out.println("5th way: " + Arrays.toString(copy5));
         
-        Integer[] sourceObjectArray = new Integer[sourceArray.length];
-        for (int i = 0; i < sourceArray.length; i++) {
-            sourceObjectArray[i] = sourceArray[i];
+        Integer[] bigNums = new Integer[myarr.length];
+        for (int i = 0; i < myarr.length; i++) {
+            bigNums[i] = myarr[i];
         }
-        List<Integer> list = new ArrayList<>(Arrays.asList(sourceObjectArray));
-        Integer[] destination6 = list.toArray(new Integer[0]);
-        System.out.println("6. ArrayList conversion: " + Arrays.toString(destination6));
+        List<Integer> mylist = new ArrayList<>(Arrays.asList(bigNums));
+        Integer[] copy6 = mylist.toArray(new Integer[0]);
+        System.out.println("6th way: " + Arrays.toString(copy6));
         
-        int[] destination7 = new int[sourceArray.length];
-        System.arraycopy(sourceArray, 0, destination7, 0, sourceArray.length);
-        System.out.println("7. Copy full array: " + Arrays.toString(destination7));
+        int[] copy7 = new int[myarr.length];
+        System.arraycopy(myarr, 0, copy7, 0, myarr.length);
+        System.out.println("7th way: " + Arrays.toString(copy7));
         
-        int[] destination8 = new int[sourceArray.length + 2];
-        System.arraycopy(sourceArray, 0, destination8, 1, sourceArray.length);
-        System.out.println("8. Copy with offset: " + Arrays.toString(destination8));
+        int[] copy8 = new int[myarr.length + 2];
+        System.arraycopy(myarr, 0, copy8, 1, myarr.length);
+        System.out.println("8th way: " + Arrays.toString(copy8));
         
-        int[] destination9 = new int[sourceArray.length];
-        if (sourceArray.length >= 0) System.arraycopy(sourceArray, 0, destination9, 0, sourceArray.length);
-        System.out.println("9. Copy with check: " + Arrays.toString(destination9));
+        int[] copy9 = new int[myarr.length];
+        if (myarr.length > 0) System.arraycopy(myarr, 0, copy9, 0, myarr.length);
+        System.out.println("9th way: " + Arrays.toString(copy9));
         
-        int[] destination10 = new int[3];
-        System.arraycopy(sourceArray, 1, destination10, 0, 3);
-        System.out.println("10. Copy subset: " + Arrays.toString(destination10));
+        int[] copy10 = new int[3];
+        System.arraycopy(myarr, 1, copy10, 0, 3);
+        System.out.println("10th way: " + Arrays.toString(copy10));
     }
-}
+                         }
