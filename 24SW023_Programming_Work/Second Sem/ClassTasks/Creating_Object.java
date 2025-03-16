3681927c234839e6d1031a053da0efc308f24398
IamAntiHero <antihero.void@gmail.com>
2025-03-16 22:02:39 +0500

    Create Creating_Object.java

---
+++ 24SW023_Programming_Work/Second Sem/ClassTasks/Creating_Object.java
@@ -0,0 +1,52 @@
+public class Person {
+    private String name;
+    private int age;
+    private String email;
+    
+    public Person(String name, int age, String email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Email: " + email);
+    }
+    
+    public static void main(String[] args) {
+        Person person = new Person("MrExample", 30, "new@example.com");
+        person.displayInfo();
+        
+        person.setAge(31);
+        person.setEmail("example@newmail.com");
+        
+        System.out.println("\nAfter update:");
+        person.displayInfo();
+    }
+}
